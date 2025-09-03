190b94ccbc76340e82e295357cc39e07405f2bbc
zeta-rpg <zckgr000@gmail.com>
2025-09-03 19:33:44 -0300

    Agregando la calculadora app

---
+++ calculator-app-main/main.js
@@ -0,0 +1,77 @@
+let numeros = document.getElementsByClassName('button');
+let screen = document.getElementsByClassName('calculator-display')[0];
+
+
+Array.from(numeros).forEach(boton => {
+    boton.addEventListener('click', () => {
+        let valor = boton.textContent;
+        
+        if (valor === 'RESET') {
+            screen.value = '';
+        }else if (valor === '=') {
+            try {
+                screen.value = eval(screen.value.replace(/x/g, '*'));
+            } catch {
+                screen.value = 'Error';
+            }   
+        }else if (valor === 'DEL') {
+            screen.value = screen.value.slice(0, -1);
+        }else {
+            screen.value += valor;
+        }
+    });
+});
+
+// Seleccionamos los inputs del switch
+const opt1 = document.getElementById('opt1');
+const opt2 = document.getElementById('opt2');
+const opt3 = document.getElementById('opt3');
+
+//Cada elemento
+const body = document.body;
+const sl = document.querySelector('span');
+const switch3 = document.querySelector('.switch-3');
+const slider = document.querySelector('.slider');
+const buttons = document.querySelectorAll('.button');
+const resetButton = document.querySelector('.reset');
+const equalButton = document.querySelector('.equal');
+const delButton = document.querySelector('.del');
+const bgButtons = document.querySelectorAll('.calculator-buttons');
+
+document.addEventListener('DOMContentLoaded', () => {
+    cambiarTema('base');
+});
+
+// Función para cambiar tema
+function cambiarTema(tema) {
+  // Quitamos cualquier clase de tema anterior
+  body.classList.remove('base', 'light', 'lile');
+  sl.classList.remove('base', 'light', 'lile');
+  switch3.classList.remove('base', 'light', 'lile');
+  slider.classList.remove('base', 'light', 'lile');
+  screen.classList.remove('base', 'light', 'lile');
+  buttons.forEach(boton => boton.classList.remove('base', 'light', 'lile'));
+  resetButton.classList.remove('base', 'light', 'lile');
+  equalButton.classList.remove('base', 'light', 'lile');
+  delButton.classList.remove('base', 'light', 'lile');
+  bgButtons.forEach(boton => boton.classList.remove('base', 'light', 'lile'));
+
+  // Agregamos la nueva clase
+  body.classList.add(tema);
+  sl.classList.add(tema);
+  switch3.classList.add(tema);
+  slider.classList.add(tema);
+  screen.classList.add(tema);
+  buttons.forEach(boton => boton.classList.add(tema));
+  resetButton.classList.add(tema);
+  equalButton.classList.add(tema);
+  delButton.classList.add(tema);
+  bgButtons.forEach(boton => boton.classList.add(tema));
+}
+
+// Eventos para cada opción
+opt1.addEventListener('change', () => { if(opt1.checked) cambiarTema('base'); });
+opt2.addEventListener('change', () => { if(opt2.checked) cambiarTema('light'); });
+opt3.addEventListener('change', () => { if(opt3.checked) cambiarTema('lile'); });
+
+
---
+++ calculator-app-main/style-guide.md
@@ -0,0 +1,97 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+> 💡 These are just the design sizes. Ensure content is responsive and meets WCAG requirements by testing the full range of screen sizes from 320px to large screens.
+
+## Colors
+
+### Theme 1
+
+#### Backgrounds
+
+- Navy 850 (main background): hsl(222, 26%, 31%)
+- Navy 900 (toggle background, keypad background): hsl(223, 31%, 20%)
+- Navy 950 (screen background): hsl(224, 36%, 15%)
+
+#### Keys
+
+- Navy 700 (key background): hsl(225, 21%, 49%)
+- Navy 800 (key shadow): hsl(224, 28%, 35%)
+
+- Red 600 (key background, toggle): hsl(6, 63%, 50%)
+- Red 800 (key shadow): hsl(6, 70%, 34%)
+
+- Gray 200 (key background): hsl(0, 0%, 90%)
+- Gray orange 400 (key shadow): hsl(28, 16%, 65%)
+
+#### Text
+
+- Navy 750: hsl(221, 14%, 31%)
+- White: hsl(0, 100%, 100%)
+
+### Theme 2
+
+#### Backgrounds
+
+- Gray 200 (main background): hsl(0, 0%, 90%)
+- Gray 300 (toggle background, keypad background): hsl(0, 5%, 81%)
+- Gray 100 (screen background): hsl(0, 0%, 93%)
+
+#### Keys
+
+- Bue 500 (key background): hsl(185, 42%, 37%)
+- Blue 600 (key shadow): hsl(185, 58%, 25%)
+
+- Orange 700 (key background, toggle): hsl(25, 98%, 40%)
+- Orange 800 (key shadow): hsl(25, 99%, 27%)
+
+- Grey 200 (key background): hsl(0, 0%, 90%)
+- Grey orange 450 (key shadow): hsl(35, 11%, 61%)
+
+#### Text
+
+- Gray 900: hsl(60, 10%, 19%)
+- White (text): hsl(0, 100%, 100%)
+
+### Theme 3
+
+#### Backgrounds
+
+- Purple 950 (main background): hsl(268, 75%, 9%)
+- Purple 900 (toggle background, keypad background, screen background): hsl(268, 71%, 12%)
+
+#### Keys
+
+- Purple 800 (key background): hsl(281, 89%, 26%)
+- Purple 400 (key shadow): hsl(285, 91%, 52%)
+
+- Cyan 500 (key background, toggle): hsl(176, 100%, 44%)
+- Cyan 400 (key shadow): hsl(177, 92%, 70%)
+
+- Purple 850 (key background): hsl(268, 47%, 21%)
+- Purple 750 (key shadow): hsl(290, 70%, 36%)
+
+#### Text
+
+- Yellow 300: hsl(52, 100%, 62%)
+- Blue 950: hsl(198, 20%, 13%)
+- White (text): hsl(0, 100%, 100%)
+
+## Typography
+
+### Body Copy
+
+- Font size (numbers): 32px
+
+### Font
+
+- Family: [League Spartan](https://fonts.google.com/specimen/League+Spartan)
+- Weights: 700
+
+> 💎 [Upgrade to Pro](https://www.frontendmentor.io/pro?ref=style-guide) for design file access to see all design details and get hands-on experience using a professional workflow with tools like Figma.
